ace0f5415313eaf244b2e581ddeae309d59b59bc
peter <peter@kungariket.se>
2023-02-25 10:39:24 +0100

    test

---
+++ main.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+
+while 1:
+    if GPIO.input(26):
+        print("Pin 26 is HIGH")
+        GPIO.output(17,GPIO.HIGH)
+    else:
+        print("Pin 26 is LOW")
+        GPIO.output(17,GPIO.LOW)
+    time.sleep(0.25)
+
+GPIO.cleanup()
